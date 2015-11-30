b48097ca35c52137992001db51236caadef69c21
shejunzhou <965678322@qq.com>
2015-11-30 17:48:00 +0800

    添加subView到父视图，约束为上下左右均为40point。
    这里比较容易犯错的地方：
    1、添加约束前确定已经把需要布局的子view添加到父view上了
    2、一定要禁止将Autoresizing Mask转换为约束
    3、要把子view的约束加在父view上
    4、因为iOS中原点在左上角，所以使用offset时注意right和bottom用负数

--- LayoutConstraintsTT/ViewController/ViewController.m
+++ LayoutConstraintsTT/ViewController/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UIView *subView;
+
 @end
 
 @implementation ViewController
@@ -17,6 +19,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self.view addSubview:self.subView];
+    NSLayoutConstraint *constrainTop = [NSLayoutConstraint constraintWithItem:_subView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0 constant:40.0];
+    NSLayoutConstraint *constrainLeft = [NSLayoutConstraint constraintWithItem:_subView attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0 constant:40.0];
+    NSLayoutConstraint *constrainBottom = [NSLayoutConstraint constraintWithItem:_subView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1.0 constant:-40.0];
+    NSLayoutConstraint *constrainRight = [NSLayoutConstraint constraintWithItem:_subView attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeRight multiplier:1.0 constant:-40.0];
+    [self.view addConstraint:constrainTop];
+    [self.view addConstraint:constrainLeft];
+    [self.view addConstraint:constrainBottom];
+    [self.view addConstraint:constrainRight];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +35,13 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (UIView *)subView {
+    if (!_subView) {
+        _subView = [[UIView alloc] init];
+        _subView.backgroundColor = [UIColor orangeColor];
+        [_subView setTranslatesAutoresizingMaskIntoConstraints:NO];
+        
+    }
+    return _subView;
+}
 @end
